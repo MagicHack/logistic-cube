fc27b3b0f4972d54ab73cd5377a0b9a417a36dba
Emile Larose-Levac <emile.larose@gmail.com>
2018-01-28 11:38:11 -0500

    Not working... cleaner code

--- data.lua
+++ data.lua
@@ -1,124 +1,167 @@
 -- Ugly mess of replacing every data.raw value (I never modded a game before and this is my first time with lua)
 -- TODO : make it cleaner (like Waitex_Full)
+--TODO : Setting to toggle
 -- Maybe make different sprites for cargo and no cargo like base game...
 -- Used 3d model from https://free3d.com/3d-model/companion-cubes-portal-8274.html to generate images in blender
 
+texturePath = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-lr.png"
+texturePathHighRes = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-hr.png"
+
+shadowPath = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-lr.png"
+shadowPathHighRes = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-hr.png"
+iconPath = "__logistic-cube__/graphics/icons/logistic-robot.png"
+
+textureLineLength = 4
+textureWidthHighRes  = 80
+textureHeightHighRes = 74
+textureWidth  = 41
+textureHeigh = 38
+
+shadowLineLength = 3
+shadowWidthHighRes  = 115
+shadowHeightHighRes = 43
+shadowWidth  = 58
+shadowHeigh = 22
+
+
+constructionBot = false
+name = "logistic-robot"
+if settings.startup["logistic-bot-construction-bot-companion-cube"].value then
+  name = "construction-robot"
+  constructionBot = true
+end
+
+robot = data.raw[name][name]
+
 -- Icon
 
-data.raw["logistic-robot"]["logistic-robot"].icon = "__logistic-cube__/graphics/icons/logistic-robot.png"
+robot.icon = iconPath
 -- ??
-data.raw.item["logistic-robot"].icon = "__logistic-cube__/graphics/icons/logistic-robot.png"
+data.raw.item[name].icon = iconPath
 -- Idle
+robotCurrent = robot.idle
 
-data.raw["logistic-robot"]["logistic-robot"].idle.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].idle.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].idle.width = 41
-data.raw["logistic-robot"]["logistic-robot"].idle.height = 38
-data.raw["logistic-robot"]["logistic-robot"].idle.y = 0
+robotCurrent.filename = texturePath
+robotCurrent.line_length = textureLineLength
+robotCurrent.width = textureWidth
+robotCurrent.height = textureHeight
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].idle.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].idle.hr_version.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].idle.hr_version.width = 80
-data.raw["logistic-robot"]["logistic-robot"].idle.hr_version.height = 74
-data.raw["logistic-robot"]["logistic-robot"].idle.hr_version.y = 0
+robotCurrent.hr_version.filename = texturePathHighRes
+robotCurrent.hr_version.line_length = textureLineLength
+robotCurrent.hr_version.width = textureWidthHighRes
+robotCurrent.hr_version.height = textureHeightHighRes
+robotCurrent.hr_version.y = 0
 
 -- Idle with cargo
+if not constructionBot then
+robotCurrent = robot.idle_with_cargo
 
-
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.width = 41
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.height = 38
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.y = 0
+robotCurrent.filename = texturePath
+robotCurrent.line_length = textureLineLength
+robotCurrent.width = textureWidth
+robotCurrent.height = textureHeight
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.hr_version.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.hr_version.width = 80
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.hr_version.height = 74
-data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.hr_version.y = 0
-
+robotCurrent.hr_version.filename = texturePathHighRes
+robotCurrent.hr_version.line_length = textureLineLength
+robotCurrent.hr_version.width = textureWidthHighRes
+robotCurrent.hr_version.height = textureHeightHighRes
+robotCurrent.hr_version.y = 0
+end
 -- In motion
 
-data.raw["logistic-robot"]["logistic-robot"].in_motion.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].in_motion.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].in_motion.width = 41
-data.raw["logistic-robot"]["logistic-robot"].in_motion.height = 38
-data.raw["logistic-robot"]["logistic-robot"].in_motion.y = 0
+
+robotCurrent = robot.in_motion
+
+robotCurrent.filename = texturePath
+robotCurrent.line_length = textureLineLength
+robotCurrent.width = textureWidth
+robotCurrent.height = textureHeight
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].in_motion.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].in_motion.hr_version.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].in_motion.hr_version.width = 80
-data.raw["logistic-robot"]["logistic-robot"].in_motion.hr_version.height = 74
-data.raw["logistic-robot"]["logistic-robot"].in_motion.hr_version.y = 0
+robotCurrent.hr_version.filename = texturePathHighRes
+robotCurrent.hr_version.line_length = textureLineLength
+robotCurrent.hr_version.width = textureWidthHighRes
+robotCurrent.hr_version.height = textureHeightHighRes
+robotCurrent.hr_version.y = 0
 
 -- In motion with cargo
+if not constructionBot then
 
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.width = 41
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.height = 38
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.y = 0
+robotCurrent = robot.in_motion_with_cargo
+
+robotCurrent.filename = texturePath
+robotCurrent.line_length = textureLineLength
+robotCurrent.width = textureWidth
+robotCurrent.height = textureHeight
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.hr_version.line_length = 4
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.hr_version.width = 80
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.hr_version.height = 74
-data.raw["logistic-robot"]["logistic-robot"].in_motion_with_cargo.hr_version.y = 0
-
+robotCurrent.hr_version.filename = texturePathHighRes
+robotCurrent.hr_version.line_length = textureLineLength
+robotCurrent.hr_version.width = textureWidthHighRes
+robotCurrent.hr_version.height = textureHeightHighRes
+robotCurrent.hr_version.y = 0
+end
 -- Shadows
 
 -- Shadow idle
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.width = 59
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.height = 22
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.y = 0
+robotCurrent = robot.shadow_idle
+
+robotCurrent.filename = shadowPath
+robotCurrent.line_length = shadowLineLength
+robotCurrent.width = shadowWidth
+robotCurrent.height = shadowHeigh
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.hr_version.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.hr_version.width = 115
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.hr_version.height = 43
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle.hr_version.y = 0
+robotCurrent.hr_version.filename = shadowPathHighRes
+robotCurrent.hr_version.line_length = shadowLineLength
+robotCurrent.hr_version.width = shadowWidthHighRes
+robotCurrent.hr_version.height = shadowHeightHighRes
+robotCurrent.hr_version.y = 0
 
 -- Shadow idle with cargo
+if not constructionBot then
+robotCurrent = robot.shadow_idle_with_cargo
 
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.width = 59
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.height = 22
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.y = 0
+robotCurrent.filename = shadowPath
+robotCurrent.line_length = shadowLineLength
+robotCurrent.width = shadowWidth
+robotCurrent.height = shadowHeigh
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.hr_version.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.hr_version.width = 115
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.hr_version.height = 43
-data.raw["logistic-robot"]["logistic-robot"].shadow_idle_with_cargo.hr_version.y = 0
-
+robotCurrent.hr_version.filename = shadowPathHighRes
+robotCurrent.hr_version.line_length = shadowLineLength
+robotCurrent.hr_version.width = shadowWidthHighRes
+robotCurrent.hr_version.height = shadowHeightHighRes
+robotCurrent.hr_version.y = 0
 -- Shadow in motion with cargo
+robotCurrent = robot.shadow_in_motion_with_cargo
 
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .width = 59
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .height = 22
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .y = 0
+robotCurrent.filename = shadowPath
+robotCurrent.line_length = shadowLineLength
+robotCurrent.width = shadowWidth
+robotCurrent.height = shadowHeigh
+robotCurrent.y = 0
 -- high resolution
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .hr_version.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .hr_version.width = 115
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .hr_version.height = 43
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion_with_cargo .hr_version.y = 0
-
+robotCurrent.hr_version.filename = shadowPathHighRes
+robotCurrent.hr_version.line_length = shadowLineLength
+robotCurrent.hr_version.width = shadowWidthHighRes
+robotCurrent.hr_version.height = shadowHeightHighRes
+robotCurrent.hr_version.y = 0
+end
 -- shadow in motion
 
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-lr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.width = 59
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.height = 22
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.y = 0
--- high resolution
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.hr_version.filename = "__logistic-cube__/graphics/entity/logistic-robot/logistic-robot-shadows-hr.png"
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.hr_version.line_length = 3
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.hr_version.width = 115
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.hr_version.height = 43
-data.raw["logistic-robot"]["logistic-robot"].shadow_in_motion.hr_version.y = 0
+robotCurrent = robot.shadow_in_motion
 
+robotCurrent.filename = shadowPath
+robotCurrent.line_length = shadowLineLength
+robotCurrent.width = shadowWidth
+robotCurrent.height = shadowHeigh
+robotCurrent.y = 0
+-- high resolution
+robotCurrent.hr_version.filename = shadowPathHighRes
+robotCurrent.hr_version.line_length = shadowLineLength
+robotCurrent.hr_version.width = shadowWidthHighRes
+robotCurrent.hr_version.height = shadowHeightHighRes
+robotCurrent.hr_version.y = 0
---
+++ settings.lua
@@ -0,0 +1,8 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "logistic-bot-construction-bot-companion-cube",
+        setting_type = "startup",
+        default_value = false
+    }
+})
